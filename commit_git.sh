f0f8de0e6179630e56afa0f647d6329e194a9c9d
raymonepping <repping@gmail.com>
2024-07-31 15:02:33 +0200

    Initial commit for medium-nosql

---
+++ commit_git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ./ || exit
+
+# Get the current date
+DD=$(date +'%d')
+MM=$(date +'%m')
+YYYY=$(date +'%Y')
+
+# Define the commit message with the current date
+COMMIT_MESSAGE="$DD/$MM/$YYYY - Updated configuration and fixed bugs"
+
+# Check the status of your working directory
+git status
+
+# Stage all changes
+git add .
+
+# Commit the changes with the dynamically generated message
+git commit -m "$COMMIT_MESSAGE"
+
+# Push changes to the 'master' branch on the remote repository
+git push origin master
